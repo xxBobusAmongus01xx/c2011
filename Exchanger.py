3107c58b12337277706f0554aa20dec6dc15a960
xxBobusAmongus01xx <yataka228@gmail.com>
2023-10-21 19:04:08 +0300

    initial commit

---
+++ Exchanger.py
@@ -0,0 +1,12 @@
+a = float(input())
+euro = 40
+dollar = 36.8
+if a==1:
+    c = a*euro
+    print(c)
+    print("It was price of euro")
+elif a==2:
+    c = a*dollar
+    print(c)
+    print("It was price of dollar")
+
